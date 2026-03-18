0301f063bdd3503624660bcc1db27196c8e51a0e
FERNANDO GAMA DE SOUSA <50833265822@info.local>
2026-03-17 21:47:40 -0300

    status

--- processamento.py
+++ processamento.py
@@ -29,4 +29,16 @@ def validar_notas(alunos):
 def calcular_media(notas):
 
     return sum(notas) / len(notas)
+
+
+def verificar_status(media):
+
+    if media >= 7:
+        return "Aprovado"
+
+    elif media >= 5:
+        return "Recuperação"
+
+    else:
+        return "Reprovado"
     
